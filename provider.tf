cb82b445234ae2678a44a2e8ea5b591f6007aafb
Avnish <mzamthabhaimzak@gmail.com>
2025-08-03 17:27:44 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+
+# we allready configure the access key and secrt key by using export command
+#here it is 
+# export AWS_SECRET_ACCESS_KEY="Your Secret key"
+# export AWS_ACCESS_KEY_ID="Your Access key"
+# echo $AWS_ACCESS_KEY_ID
+# echo $AWS_SECRET_ACCESS_KEY
