c8e7179e05c3d046679ec15818d3bf6c68a43b5f
Avnish <mzamthabhaimzak@gmail.com>
2025-08-04 16:26:59 +0530

    Update installHttpd.sh

--- installHttpd.sh
+++ installHttpd.sh
@@ -7,3 +7,8 @@ systemctl enable apache2
 cat <<EOF > /var/www/html/index.html
 <html><h1>Hey Avnish Here</h1></html>
 EOF
+
+
+# make sure in your security group http port 80 is open 
+// hit that url in browser 
+# http://<your instances >
